ca4a852229b5bad0009ce25d809f908c2780978f
epochqwert <epoch@hacking.allowed.org>
2015-05-18 06:08:56 -0500

    changed the use of wc -l to grep -c '^.*$' because for some reasons Linux and BSD can't agree on how to space the output.

--- genheader.sh
+++ genheader.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-L=$(wc -l libhashtable.c | tr -s ' ' | cut '-d ' -f2)
+L=$(grep -c '^.*$' libhashtable.c)
 cat libhashtable.c | grep -A "$L" _A_ | grep -B "$L" _B_ | grep -v "_[AB]_" > hashtable.h
 cat libhashtable.c | grep '(.*) *{' | egrep -v 'switch|if|for|while' | sed 's/ {/;/' >> hashtable.h
